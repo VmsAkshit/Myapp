c6484fd739a5657516b95eca73197cccab46e813
Akshit <am5859241@gmail.com>
2025-11-18 15:21:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+// index.js - Cleaned up
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const container = document.getElementById('root');
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+    const root = ReactDOM.createRoot(rootElement);
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    );
+} else {
+    console.error("Error: Could not find element with ID 'root'.");
+}
